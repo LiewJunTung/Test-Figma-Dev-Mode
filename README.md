7341343bab95d18774f639d9cd76e0a23f9bb776
Liew Jun Tung <juntung@netvirta.com>
2024-10-22 10:12:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# testing_figma_dev_mode
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/testing_figma_dev_mode/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.testing_figma_dev_mode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
